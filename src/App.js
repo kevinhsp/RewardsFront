99927506b4c89cfcfd63d333a7e24ae3bc3c8a89
hesupeng <hsp1805686536@gmail.com>
2023-11-15 15:19:32 -0800

    Add month year

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import UserPurchases from './UserPurchases';
 import UpdatePurchaseForm from './UpdatePurchaseForm';
 import DeletePurchaseButton from './DeletePurchaseButton';
 import UserInfo from "./UserInfo";
+import YearMonthPurchases from "./YearMonthPurchases";
 
 function App() {
     const [userId, setUserId] = useState('');
     const [purchaseId, setPurchaseId] = useState('');
     const [refresh, setRefresh] = useState(false);
+    const [year, setYear] = useState(new Date().getFullYear().toString());
+    const [month, setMonth] = useState((new Date().getMonth() + 1).toString());
 
     const handleActionComplete = () => {
         setRefresh(prev => !prev);
@@ -27,6 +30,20 @@ function App() {
             <UserInfo userId={userId} refresh = {refresh} />
             <UserPurchases userId={userId} refresh={refresh} />
             <PurchaseForm userId={userId} onPurchaseCreated={handleActionComplete} />
+            <h2>Purchases in Specific Month</h2>
+            <input
+                type="text"
+                value={year}
+                onChange={(e) => setYear(e.target.value)}
+                placeholder="Year"
+            />
+            <input
+                type="text"
+                value={month}
+                onChange={(e) => setMonth(e.target.value)}
+                placeholder="Month"
+            />
+            <YearMonthPurchases userId={userId} year={year} month={month} />
 
             <input
                 type="text"
